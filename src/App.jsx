438ceb1cefdf6696b3b4afbcb69d80cbe3e41b98
juuuunny <102502542+juuuunny@users.noreply.github.com>
2023-11-16 23:19:14 +0900

    [feat]추가정보 입력페이지 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import AdditionRole from "./pages/addition";
 import PlusAddition from "./pages/addition/selectedrole";
 
@@ -9,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AdditionRole />}></Route>
+        <Route path="/addition" element={<AdditionRole />}></Route>
         <Route path="/addition/detail" element={<PlusAddition />}></Route>
       </Routes>
     </Router>
