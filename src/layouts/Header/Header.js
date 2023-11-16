7e3188ea9cff4dc713f011ab827f5fc0262dabf2
WONYOUNG-HC <whdnjsdud551@gmail.com>
2023-11-16 10:39:46 +0900

    [feat] 임시헤더(로고) 구현

---
+++ src/layouts/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Logo } from "./styles";
+
+const Header = () => {
+  return <Logo>Fellow</Logo>;
+};
+
+export default Header;
---
+++ src/layouts/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Logo = styled.div`
+  color: var(--Main-Color, #809f80);
+  text-align: center;
+  font-family: Coiny;
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 0.7px;
+`;
