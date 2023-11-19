7af1c00f817a854b13ab8c08622f6faa0b91c1cd
yujeongFE <yujeong6231@naver.com>
2023-11-19 21:43:01 +0900

    [feat] 배경 화면 ui 구현

---
+++ src/components/BackGround.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const LogoContainer = styled.div`
+  margin-top: 30px;
+  color: #809f80;
+  text-align: center;
+  font-family: "Coiny";
+  font-size: 35px;
+  font-weight: 400;
+  letter-spacing: 0.7px;
+  z-index: 1;
+`;
+
+const Pattern = styled.div`
+  z-index: 0;
+  width: 31.26vw;
+  height: 31.26vw;
+  max-width: 602px;
+  max-height: 602px;
+  min-width: 200px;
+  min-height: 200px;
+  position: absolute;
+  background-color: ${(props) =>
+    props.color === "brown"
+      ? "rgba(215, 171, 110, 0.4)"
+      : props.color === "green"
+      ? "rgba(128, 159, 128, 0.40)"
+      : "default_color, default_opacity"};
+  filter: blur(10.42vw);
+
+  ${(props) =>
+    props.color === "green" &&
+    css`
+      top: 4.44vh;
+      left: -14.74vw;
+    `}
+  ${(props) =>
+    props.color === "brown" &&
+    css`
+      width: 25.26vw;
+      height: 35.9vh;
+      right: -10.26vw;
+      bottom: -5.19vh;
+    `}
+`;
+
+const Background = () => {
+  return (
+    <>
+      <LogoContainer>Feelow</LogoContainer>
+      <Pattern color="brown" />
+      <Pattern color="green" />
+    </>
+  );
+};
+
+export default Background;
