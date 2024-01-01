2e2d110915f0ba4866729da2e7b0952cd80c2e77
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-01-01 21:22:28 +0900

    [feat] 마이페이지 기본 디자인 틀 구축

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-import { AdditionTotalBox, FlexBox, LogoName, UserInfoBox } from "./style";
+import {
+  AdditionTotalBox,
+  FlexBox,
+  Img,
+  InfoDiv,
+  InfoInput,
+  InfoLabel,
+  LogoName,
+  Placeholder,
+  UserInfoBox,
+} from "./style";
+import characterImg from "../../assets/character_img.svg";
+import circleImg from "../../assets/circle_img.svg";
 
 const MyPage = () => {
   return (
@@ -11,7 +23,54 @@ const MyPage = () => {
             <div style={{ height: "40px" }}>상단 메뉴바 가져와서 추가할 계획</div>
           </FlexBox>
         </AdditionTotalBox>
-        <UserInfoBox></UserInfoBox>
+        <UserInfoBox>
+          <FlexBox>
+            <Img
+              Top="220px"
+              width="210px"
+              height="255px"
+              zIndex="2"
+              margin="0px 0px 0px -20px"
+              src={characterImg}
+              alt="character"
+            />
+            <Img src={circleImg} alt="character" />
+          </FlexBox>
+          <InfoDiv margin="410px 0px 0px 0px">
+            <InfoLabel>학교</InfoLabel>
+            <InfoInput>서울대학교사범대학부설고등학교</InfoInput>
+          </InfoDiv>
+          <InfoDiv margin="33px 0px 0px 0px">
+            <InfoLabel>학년/반/번호</InfoLabel>
+            <InfoInput justifyContent="space-around" width="32%" maxWidth="63px">
+              2<Placeholder>학년</Placeholder>
+            </InfoInput>
+            <InfoInput
+              justifyContent="space-around"
+              margin="0px 0px 0px 6px"
+              width="32%"
+              maxWidth="63px"
+            >
+              2<Placeholder>반</Placeholder>
+            </InfoInput>
+            <InfoInput
+              justifyContent="space-around"
+              margin="0px 0px 0px 6px"
+              width="32%"
+              maxWidth="63px"
+            >
+              2<Placeholder>번호</Placeholder>
+            </InfoInput>
+          </InfoDiv>
+          <InfoDiv margin="33px 0px 0px 0px">
+            <InfoLabel>이름</InfoLabel>
+            <InfoInput>강희수</InfoInput>
+          </InfoDiv>
+          <InfoDiv margin="33px 0px 0px 0px">
+            <InfoLabel>닉네임</InfoLabel>
+            <InfoInput>빛나는물</InfoInput>
+          </InfoDiv>
+        </UserInfoBox>
       </FlexBox>
     </>
   );
--- src/pages/mypage/style.jsx
+++ src/pages/mypage/style.jsx
@@ -20,8 +20,63 @@ export const LogoName = styled.div`
   font-weight: 400;
   letter-spacing: 0.7px;
 `;
+
 export const UserInfoBox = styled.div`
   width: 100%;
   height: 100vh;
   background-color: #f7f0ea;
+`;
+
+export const Img = styled.img`
+  position: absolute;
+  width: ${(props) => props.width || "279px"};
+  height: ${(props) => props.height || "279px"};
+  z-index: ${(props) => props.zIndex || "1"};
+  top: ${(props) => props.Top || "180px"};
+  margin: ${(props) => props.margin || "0px"};
+`;
+
+export const InfoDiv = styled.div`
+  width: 96%;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: ${(props) => props.margin || "0px"};
+`;
+
+export const InfoLabel = styled.label`
+  max-width: 113px;
+  margin: ${(props) => props.margin || "0px 30px 0px 0px"};
+  padding-left: 18px;
+  width: 48%;
+  height: 26px;
+  color: var(--Main-Font, #2c2c2c);
+  font-family: "Pretendard";
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+export const InfoInput = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => props.justifyContent || "none"};
+  max-width: ${(props) => props.maxWidth || "258px"};
+  width: ${(props) => props.width || "140%"};
+  height: 43px;
+  padding: 0px 0px 0px 24px;
+  border-radius: 20px;
+  margin: ${(props) => props.margin || "0px"};
+  border: 2px solid #809f80;
+  background: #fff;
+  color: var(--Main-Font, #2c2c2c);
+  font-family: "Pretendard";
+`;
+
+export const Placeholder = styled.div`
+  color: var(--Sub-Font, #b0b0b0);
+  margin: "0px 0px 0px 5px";
+  font-family: "Pretendard";
+  font-size: 14px;
+  margin: 0px 6px 0px 0px;
 `;
