f127dc09570e06fd21c644f6fea0be0c3563ec91
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-02-11 17:51:44 +0900

    [fix] 리액트 캘랜더 오류 수정 및 불필요한 코드 삭제

--- src/pages/teacher/index.jsx
+++ src/pages/teacher/index.jsx
@@ -11,7 +11,6 @@ import {
 import ButtonBox from "../../layouts/ButtonBox";
 import { useStateManager } from "react-select";
 import { GetStudentListApi } from "../../utils/Teacher";
-import character_img from "../../assets/feelow_character.png";
 import { StudentDiv } from "./component/StudentDiv";
 import Pagination from "react-js-pagination";
 import "./paging.css";
